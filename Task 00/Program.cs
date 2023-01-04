3e0bf1e918beb95a5ee8b44ae7af851df5b35078
unknown <you@example.com>
2023-01-04 13:02:21 +0300

    Add program.cs

--- Task 00/Program.cs
+++ Task 00/Program.cs
@@ -1,6 +1,17 @@
-﻿int number = Convert.ToInt32(Console.ReadLine());
-string str = Console.ReadLine();
-int number = Convert.ToInt32(str);
-square = number * number;
-Console.WriteLine(square);
-Console.WriteLine($"Квадрат числа {number} = {square}");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Main(args, v);
+    }
+
+    private static void Main(string[] args, int number1, int square)
+    {
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        string str = Console.ReadLine();
+        int v = Convert.ToInt32(str);
+        square = number1 * number1;
+        Console.WriteLine(square);
+        Console.WriteLine($"Квадрат числа {number1} = {square}");
+    }
+}
